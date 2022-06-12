7017a6d516ae6c09bfa5bf547bdb4154a1a7453d
David Garza <garzadavid1108@gmail.com>
2022-06-12 15:29:21 -0700

    setting up project

--- tourneysite-project/register/views.py
+++ tourneysite-project/register/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+from django.contrib.auth import login, logout, authenticate
+
 
 # Create your views here.
+def register(request) :
+    return
--- tourneysite-project/tourneysite/urls.py
+++ tourneysite-project/tourneysite/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('register/', include('register.urls')),
+    path('user/', include('register.urls')),
 ]
