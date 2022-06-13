2308b2a93f27f779716a607a0c907352cc89c760
David Garza <garzadavid1108@gmail.com>
2022-06-13 13:08:43 -0700

    register view done
    Reminder: needs bootstrap

--- tourneysite-project/register/views.py
+++ tourneysite-project/register/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.db import IntegrityError
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.contrib.auth import login, logout, authenticate
@@ -8,3 +9,18 @@ from django.contrib.auth import login, logout, authenticate
 def register(request) :
     if request.method == 'GET' :
         return render(request, 'register/register.html', {'form': UserCreationForm()})
+    else :
+        if request.POST['password1'] == request.POST['password2'] :
+            try :
+                user = User.objects.create_user(request.POST['username'],
+                    password=request.POST['password1'])
+                user.save() 
+                login(request, user)
+
+                return redirect('dashboard')
+            except IntegrityError :
+                return render(request, 'register/register.html', 
+                {'form': UserCreationForm(), 'error': 'That username is taken'})
+        else :
+            return render(request, 'register/register.html', 
+            {'form': UserCreationForm(), 'error': 'Passwords dont match'})
