9d83912d1dfd8f81d5342baf6990231ba8c79363
David Garza <garzadavid1108@gmail.com>
2022-06-13 14:33:55 -0700

    working on user login

--- tourneysite-project/register/urls.py
+++ tourneysite-project/register/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('register/', views.register, name='register'),
-    # path('login/', views.login, name='login'),
+    path('user_login/', views.user_login, name='userLogin'),
     # path('logout/', views.logout, name='logout'),
 ]
--- tourneysite-project/register/views.py
+++ tourneysite-project/register/views.py
@@ -24,3 +24,18 @@ def register(request) :
         else :
             return render(request, 'register/register.html', 
             {'form': UserCreationForm(), 'error': 'Passwords dont match'})
+
+    def user_login(request) :
+        if request.method == 'GET' :
+            return render(request, 'register/login_user.html', 
+            {'form': AuthenticationForm()})
+        else :
+            user = authenticate(request, username=request.POST['username'], 
+            password=request.POST['password'])
+            if user is None :
+                return render(request, 'register/login_user.html', 
+                {'form': AuthenticationForm(), 'error': 'Username and password did not match'})
+            else :
+                login(request, user)
+
+                return redirect('dashboard')
