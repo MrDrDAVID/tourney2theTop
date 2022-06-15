82b96f6696ddaa028e5b39465694a8446248236a
David Garza <garzadavid1108@gmail.com>
2022-06-15 16:58:44 -0700

    register models in admin

--- tourneysite-project/tournament/admin.py
+++ tourneysite-project/tournament/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Tournament, Participants, Matches
 
 # Register your models here.
+admin.site.register(Tournament)
+admin.site.register(Participants)
+admin.site.register(Matches)
