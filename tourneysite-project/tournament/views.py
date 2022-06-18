4733bfe98b24630a8727ec7c38998ae4cbae2dc1
David Garza <garzadavid1108@gmail.com>
2022-06-18 15:48:33 -0700

    create_tournament view
    also removed forms.py as it was not needed

--- tourneysite-project/tournament/views.py
+++ tourneysite-project/tournament/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def dashboard(request) :
     return render(request, 'tournament/dashboard.html')
+
+def create_tournament(request) :
+    return render(request, 'tournament/create_tournament.html')
